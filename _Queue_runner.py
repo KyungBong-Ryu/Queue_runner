4cd91d48f57625061c6af1472f833e4fcd899bc1
KyungBong-Ryu <103981195+KyungBong-Ryu@users.noreply.github.com>
2023-02-20 14:32:27 +0900

    v 1.1
    log 파일 이름 및 기록 양식 수정

--- _Queue_runner.py
+++ _Queue_runner.py
@@ -9,8 +9,10 @@ def logger(path_file, in_str):
 
 
 if __name__ == "__main__":
+    _str = str(time.strftime('%Y_%m_%d_%H_%M_%S'))
+    
     path_q_list = "./_Queue_list.txt"   # Queue list. 실행할 py 파일 이름 + 확장자를 한 줄에 하나씩 적어두기
-    log_q_runner = "./_Queue_log.txt"   # Queue_runner 실행기록. 파일 없으면 생성후 기록함
+    log_q_runner = "./_Queue_log_" + _str + ".txt"   # Queue_runner 실행기록. 파일 없으면 생성후 기록함
     
     list_finished = []
     
@@ -32,9 +34,11 @@ if __name__ == "__main__":
                     q_name = q_list_line.strip("\n")
                     
                     if q_name in list_finished:
-                        _str = "===[ WARNING: " + q_name + " passed... It has already done ]==="
+                        _str = "===[ WARNING: ( " + q_name + " ) passed... It has already done ]==="
                         logger(log_q_runner, _str)
                     else:
+                        _str = "===[ Try running... ( " + q_name + " ) ]==="
+                        logger(log_q_runner, _str)
                         try:
                             is_py = q_name.split(".")[-1]
                             if is_py == "py":
@@ -45,9 +49,9 @@ if __name__ == "__main__":
                                     list_finished.append(q_name)
                                     _str = "===[ Success: ( " + q_name + " ) ]==="
                                 else:
-                                    _str = "***[ FAILURE: Can not run " + q_name + " ]***"
+                                    _str = "***[ FAILURE: Can not run ( " + q_name + " ) ]***"
                             else:
-                                _str = "***[ FAILURE: " + q_name + " is not a .py file ]***"
+                                _str = "***[ FAILURE: ( " + q_name + " ) is not a .py file ]***"
                             
                             logger(log_q_runner, _str)
                         except:
