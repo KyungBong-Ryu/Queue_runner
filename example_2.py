71bd39f3da860fd99f93a93306611e1129cf402a
KyungBong-Ryu <103981195+KyungBong-Ryu@users.noreply.github.com>
2023-02-21 16:40:56 +0900

    v 1.3
    argparse 지원

--- example_2.py
+++ example_2.py
@@ -1,7 +1,13 @@
 import time
-
+import argparse
 if __name__ == "__main__":
-    _str = "b"
+    parser = argparse.ArgumentParser(description='Argparse Tutorial')
+    parser.add_argument("--epoch",  type=int, default=200)
+    parser.add_argument("--name", type=str, default="example_2")
+    args = parser.parse_args()
+    
+    _str = args.name
     print("\ninit", _str)
+    print("epoch =", args.epoch)
     time.sleep(1)
-    print("\nfinish", _str)
+    print("finish", _str)
